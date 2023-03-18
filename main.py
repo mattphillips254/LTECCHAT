c61914c5fafb7bf03715fcc1c1016f7ff97d69bf
mattphillips254 <104286890+mattphillips254@users.noreply.github.com>
2023-03-18 09:56:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,5 @@ def predict():
    return jsonify(message)
 
 if __name__ == "__main__":
-    app.run(port=int(os.environ.get("PORT", 3000)),host='0.0.0.0',debug=True)
+   app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+
